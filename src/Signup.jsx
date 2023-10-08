20be125f208f1d08df432f315867af7db888377c
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-10-08 23:18:08 +0330

    change structure inputs setstate

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,30 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import validation from './Validation'
 export default function Signup() {
-    const [inputName,setName]=useState('')
-    const [inputEmail,setEmail]=useState('')
-    const [inputPassword,setPassword]=useState('')
-    const [inputConfirm,setConfirm]=useState('')
-    const [inputCheckbox,setCheckbox]=useState(false)
+  const [data,setData]=useState({
+    inputName:'',
+    inputEmail:'',
+    inputPassword:'',
+    inputConfirm:'',
+    inputCheckbox:false,
+  })
     
+    const changeInputHandler=(e)=>{
+      if(e.target.name=='inputCheckbox'){
+        setData({
+          ...data,[e.target.name]:e.target.checked
+        })
+
+      }else{
+        setData({
+        ...data,[e.target.name]:e.target.value
+        })
+      }
+    }
   return (
     <>
         <div className="container">
         <div className="singup">
         <h1>Sing Up</h1>
-        <label htmlFor="name">Name</label>{inputName}
-        <input type="text" placeholder='Enter Name...' onChange={(e)=>{setName(e.target.value);validation(inputName)}}  value={inputName}/>
-        <label htmlFor="email">Email</label>{inputEmail}
-        <input type="text" placeholder='Enter Email...' onChange={(e)=>setEmail(e.target.value)} value={inputEmail}/>
+        <label htmlFor="name">Name</label>{data.inputName}
+        <input type="text" placeholder='Enter Name...' onChange={changeInputHandler}  value={data.inputName} name='inputName' />
+
+        <label htmlFor="email">Email</label>{data.inputEmail}
+        <input type="text" placeholder='Enter Email...' onChange={changeInputHandler} value={data.inputEmail} name='inputEmail' />
 
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder='Enter Password...' onChange={(e)=>setPassword(e.target.value)} value={inputPassword} name='setPassword'/>
+        <input type="password" placeholder='Enter Password...' onChange={changeInputHandler} value={data.inputPassword} name='inputPassword'/>
 
         <label htmlFor="ConfirmPassword">Confirm Password</label>
-        <input type="password" placeholder='Enter Confirm Password...' onChange={(e)=>setConfirm(e.target.value)} value={inputConfirm} name='setConfirm'/>
+        <input type="password" placeholder='Enter Confirm Password...' onChange={changeInputHandler} value={data.inputConfirm} name='inputConfirm'/>
+
         <div className='checkbox'>
           <label htmlFor="policy">accept terms of privacy policy</label>
-          <input type="checkbox" id="policy" onChange={()=>setCheckbox(!inputCheckbox)} value={inputCheckbox} name='setCheckbox'/>
+          <input type="checkbox" id="policy" onChange={changeInputHandler} value={data.inputCheckbox} name='inputCheckbox'/>
         </div>
         <div className="btns">
           <span>Login</span>
