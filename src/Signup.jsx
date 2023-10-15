bcd3bd501ab22fdae05fd15e896975519dde7c9d
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2023-10-15 20:00:22 +0330

    complate form signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { validation } from './validation'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css"
+import {notify} from "./toast.js"
 export default function Signup() {
   const [data, setData] = useState({
     inputName: '',
@@ -11,8 +15,8 @@ export default function Signup() {
   const [focus, setFocus] = useState({
     inputName: false,
     inputEmail: false,
-    inputConfirm: false,
     inputPassword: false,
+    inputConfirm: false,
     inputCheckbox: false,
   })
   const [error, setError] = useState({})
@@ -22,7 +26,6 @@ export default function Signup() {
       setData({
         ...data, [e.target.name]: e.target.checked
       })
-
     } else {
       setData({
         ...data, [e.target.name]: e.target.value
@@ -30,10 +33,26 @@ export default function Signup() {
     }
   }
 
+
   const focusHandler = (e) => {
     setFocus({
       ...focus,[e.target.name]:true
-    })
+    })     
+    console.log(focus);
+  }
+
+  
+
+  const submitHandler = event =>{
+    event.preventDefault();
+    if(!Object.keys(error).length){
+      notify('so u are succesfuly input send','success')
+    }else{
+      setFocus({
+        inputName:true,inputEmail:true,inputConfirm:true,inputPassword:true,inputCheckbox:true
+      })
+      notify('please check your inputs','error')
+    }
   }
 
   useEffect(() => {
@@ -43,35 +62,37 @@ export default function Signup() {
   return (
     <>
       <div className="container">
-        <div className="singup">
+      <ToastContainer />
+        <form className="singup">
           <h1>Sing Up</h1>
           <label htmlFor="name">Name</label>
-          <input type="text" placeholder='Enter Name...' onFocus={focusHandler} onChange={changeInputHandler} value={data.inputName} name='inputName' />
-          {focus.inputName && error.name ? <span>{error.name}</span> : ""}
+          <input type="text" placeholder='Enter Name...' className={(focus.inputName && error.name) ? "errorinput": "successinput"} onFocus={focusHandler} onChange={changeInputHandler} value={data.inputName} name='inputName' />
+          {focus.inputName && error.name ? <span className='span'>{error.name}</span> : ""}
 
           <label htmlFor="email">Email</label>
-          <input type="text" placeholder='Enter Email...' onFocus={focusHandler} onChange={changeInputHandler} value={data.inputEmail} name='inputEmail' />
-          {focus.inputEmail && error.email ? <span>{error.email}</span> : ""}
+          <input type="text" placeholder='Enter Email...' className={(focus.inputEmail && error.email) ? "errorinput":'successinput'} onFocus={focusHandler} onChange={changeInputHandler} value={data.inputEmail} name='inputEmail' />
+          {focus.inputEmail && error.email ? <span className='span'>{error.email}</span> : ""}
           
           <label htmlFor="password">Password</label>
-          <input type="password" placeholder='Enter Password...' onFocus={focusHandler} onChange={changeInputHandler} value={data.inputPassword} name='inputPassword' />
-          {focus.inputPassword && error.password ? <span>{error.password}</span> : ""}
+          <input type="password" placeholder='Enter Password...'className={(focus.inputPassword && error.password) ? "errorinput":'successinput'} onFocus={focusHandler} onChange={changeInputHandler} value={data.inputPassword} name='inputPassword' />
+          {focus.inputPassword && error.password ? <span className='span'>{error.password}</span> : ""}
 
           <label htmlFor="ConfirmPassword">Confirm Password</label>
-          <input type="password" placeholder='Enter Confirm Password...' onFocus={focusHandler} onChange={changeInputHandler} value={data.inputConfirm} name='inputConfirm' />
-          {focus.inputConfirm && error.comfirm ? <span>{error.comfirm}</span> : ""}
+          <input type="password" placeholder='Enter Confirm Password...' className={(focus.inputConfirm && error.comfirm) ? "errorinput":'successinput'} onFocus={focusHandler} onChange={changeInputHandler} value={data.inputConfirm} name='inputConfirm' />
+          {focus.inputConfirm && error.comfirm ? <span className='span'>{error.comfirm}</span> : ""}
 
           <div className='checkbox'>
             <label htmlFor="policy">accept terms of privacy policy</label>
             <input type="checkbox" id="policy" onFocus={focusHandler} onChange={changeInputHandler} value={data.inputCheckbox} name='inputCheckbox' />
-            {focus.inputCheckbox && error.checkbox ? <span>{error.checkbox}</span> : ""}
+            {focus.inputCheckbox && error.checkbox ? <span className='span'>{error.checkbox}</span> : ""}
 
           </div>
           <div className="btns">
             <span>Login</span>
             <button>Sing Up</button>
           </div>
-        </div>
+          <input type="submit" value="Submit" onClick={submitHandler}/>
+        </form>
       </div>
     </>
   )
